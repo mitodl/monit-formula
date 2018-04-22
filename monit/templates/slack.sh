47361539b80f511927fa118fca1e677e8d7bd98e
Sar <sar.haidar@gmail.com>
2018-04-22 19:20:36 -0400

    Update slack.sh

--- monit/templates/slack.sh
+++ monit/templates/slack.sh
@@ -20,4 +20,4 @@ PAYLOAD="{
   ]
 }"
 
-curl -s -X POST -H "application/json" -d "payload=$PAYLOAD" {{ slack_webhook_url }}
+curl -s -X POST -H "application/json" -d "payload=$PAYLOAD" {{ slack_mitx_eng_alerts }}
